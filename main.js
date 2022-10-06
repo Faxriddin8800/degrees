5a4d7b1dda16937074102ede2eea725cf767366e
faxriddin <fax@mail.ru>
2022-10-07 02:26:28 +0500

    dag

---
+++ main.js
@@ -0,0 +1,8 @@
+var a = +prompt("raqam kiriting")
+var b = +prompt("raqam darajasini kiriting")
+   if (a ** b){
+    art = a ** b
+    alert( art )
+    }else if (a + isNaN || b + isNaN) {
+        alert("(boshqatdan kirgizing)     siklda qila olmadim o'rgating iltimos !!! :(" )
+    }
